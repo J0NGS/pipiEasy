84f15ff6d461afdf18ead8dcabc9517c639d510a
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-08-09 23:27:14 -0300

    init

---
+++ src/main/java/com/devCry/pipiEasy/PipiEasyApplication.java
@@ -0,0 +1,13 @@
+package com.devCry.pipiEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipiEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipiEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devCry/pipiEasy/PipiEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devCry.pipiEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipiEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
